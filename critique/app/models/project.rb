d2f919450a5671152d4899f844b4361d1893eee3
Edmund Li <edmund.xz.lee@gmail.com>
2015-01-30 15:26:19 -0800

    added error messages partial

--- critique/app/models/project.rb
+++ critique/app/models/project.rb
@@ -1,11 +1,11 @@
 class Project < ActiveRecord::Base
   belongs_to :user
 
-  validates :name, presence: true
+  validates(:name, presence: true, length: { maximum: 100, minimum: 5 })
 
-  validates :type, presence: true
+  #validates :type, presence: true
 
-  validates :location, allow_blank: true
+  #validates :location, allow_blank: true
 
-  validates :description, allow_blank: true
+  #validates :description, allow_blank: true
 end
