eb357ef04b5953def509b0321f650d860364902d
Edmund Li <edmund.xz.lee@gmail.com>
2015-01-30 15:18:05 -0800

    migrated db, added project resources to routes

--- critique/config/routes.rb
+++ critique/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/help'
 
   devise_for :users
+  resources :projects
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
