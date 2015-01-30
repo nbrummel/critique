16fa99fe291590523fa6aa40831925bc4ff83402
Edmund Li <edmund.xz.lee@gmail.com>
2015-01-29 21:50:39 -0800

    migrated

--- critique/app/models/project.rb
+++ critique/app/models/project.rb
@@ -1,3 +1,11 @@
 class Project < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, presence: true
+
+  validates :type, presence: true
+
+  validates :location, allow_blank: true
+
+  validates :description, allow_blank: true
 end
