c8c7373596edd1bf8d9ee9c1ac4fd8263133ed44
Nathan Brummel <sabersimon@gmail.com>
2015-01-30 13:24:47 -0800

    Created usercontroller and the views associated with it

---
+++ critique/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def search
+  end
+
+  def destroy
+  end
+
+  def dashboard
+  end
+end
