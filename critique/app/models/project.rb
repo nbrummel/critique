cd251e0d17db148733c12ac830f90b5d85a5a26e
Edmund Li <edmund.xz.lee@gmail.com>
2015-01-30 15:29:18 -0800

    improved validations for project model

--- critique/app/models/project.rb
+++ critique/app/models/project.rb
@@ -3,9 +3,9 @@ class Project < ActiveRecord::Base
 
   validates(:name, presence: true, length: { maximum: 100, minimum: 5 })
 
-  #validates :type, presence: true
+  validates(:type, presence: true, length: { maximum: 25,  minimum: 3 })
 
-  #validates :location, allow_blank: true
+  validates(:location,    length: { maximum: 50 },   allow_blank: true)
 
-  #validates :description, allow_blank: true
+  validates(:description, length: { maximum: 2000 }, allow_blank: true)
 end
