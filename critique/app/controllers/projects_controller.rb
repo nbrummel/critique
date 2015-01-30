eb1aa5de38ae1fa0e5ce638c97a25809b62aeee6
Edmund Li <edmund.xz.lee@gmail.com>
2015-01-29 23:47:18 -0800

    modified def create in project_controllers, added def project_params private method to project posting

--- critique/app/controllers/projects_controller.rb
+++ critique/app/controllers/projects_controller.rb
@@ -9,13 +9,16 @@ class ProjectsController < ApplicationController
   end
 
   def new
-	  @project = project.new
+	  @project = Project.new
   end
 
   def create
-	  @project = project.new(params[:project])
+	  @project = Project.new(project_params)
     if @project.save
+      flash[:success] = "New project created"
 	    redirect_to :action => 'list'
+    else
+      render 'new'
     end
    end
 
@@ -29,5 +32,10 @@ class ProjectsController < ApplicationController
   def delete
     project.find(params[:id]).destroy
     redirect_to :action => 'list'
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :type, :location, :description)
   end
 end
