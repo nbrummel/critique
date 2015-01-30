5cc5d81d1eb31fa9e5beabf6da8a4f49615d336d
kangkyu <kangkyu1111@gmail.com>
2015-01-29 21:49:40 -0800

    schema updated after merging model branch

--- critique/db/schema.rb
+++ critique/db/schema.rb
@@ -11,20 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130050340) do
+ActiveRecord::Schema.define(version: 20150130050730) do
 
   create_table "reviews", force: :cascade do |t|
     t.string   "name"
-    t.integer  "user_id"
-    t.integer  "project_id"
     t.text     "description"
     t.float    "rating"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
-  add_index "reviews", ["project_id"], name: "index_reviews_on_project_id"
-  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
