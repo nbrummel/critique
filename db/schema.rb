65ee8c7ad0f9b19063cfdc33b4991b8c0d270b3d
Edmund Li <edmund.xz.lee@gmail.com>
2015-02-24 20:43:10 -0800

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225033416) do
+ActiveRecord::Schema.define(version: 20150225044037) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 20150225033416) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.float    "rating"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "title"
+    t.text     "comment"
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
