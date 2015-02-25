4aa119c61a67c72e9011982abcfe4c0626cf5bde
Edmund Li <edmund.xz.lee@gmail.com>
2015-02-24 21:26:43 -0800

    added name column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225044037) do
+ActiveRecord::Schema.define(version: 20150225052030) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150225044037) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
