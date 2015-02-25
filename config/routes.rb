be57d77088afae4b922aaff80c78153c93ce7c8b
Edmund Li <edmund.xz.lee@gmail.com>
2015-02-24 20:40:56 -0800

    added review scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :projects
+  resources :reviews
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
