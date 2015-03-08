05c633f07d1d31c9360e9ba3d015481e0f588949
Edmund Li <edmund.xz.lee@gmail.com>
2015-03-08 10:52:49 -0700

    forget to add schema file from last commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306064058) do
+ActiveRecord::Schema.define(version: 20150306070050) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -31,7 +31,12 @@ ActiveRecord::Schema.define(version: 20150306064058) do
     t.integer  "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "project_id"
   end
+
+  add_index "reviews", ["project_id"], name: "index_reviews_on_project_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
